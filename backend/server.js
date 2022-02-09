97710957fabeca0d8b5f21c7cb5b57a2d7e6e041
Sean Morgan <insideseanshead@gmail.com>
2022-02-09 10:06:09 -0800

    set up server.js

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/character", require("./routes/characterRoutes"));
+
+
+
+app.listen(port, ()=>console.log(`server started on port ${port}`))
